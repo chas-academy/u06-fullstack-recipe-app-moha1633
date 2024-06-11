08bba9f76884e8814c9eda9b17d56bfcf989cc8d
mohamed abdi <mgudaal6@gmail.com>
2024-06-11 19:17:18 +0000

    create some  empty fo

---
+++ recipe-frontend/src/app/recipes/specific-recipe/specific-recipe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-specific-recipe',
+  standalone: true,
+  imports: [],
+  templateUrl: './specific-recipe.component.html',
+  styleUrl: './specific-recipe.component.scss'
+})
+export class SpecificRecipeComponent {
+
+}
